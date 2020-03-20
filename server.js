6ad275eb63abc4136c57beec28706822742a9385
Dominic Bridgette <54685761+Afrodo1@users.noreply.github.com>
2020-03-19 19:21:53 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const server = express();
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
+
+server.use(express.json());
+
+server.use(logger);
+
+// Routers
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+//custom middleware
+
+function logger(req, res, next) {
+  const method = req.method;
+	const url = req.originalUrl;
+	const timeStamp = Date.now();
+
+	console.log(`\nMethod: ${method} \nUrl: ${url} \nTime: ${timeStamp}`);
+
+	next();
+}
+
+module.exports = server;
